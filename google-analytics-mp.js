e815ac0f1188b92f9413d795d6d41e3e44c2ae78
Ken Ono <ken.ono.gm@gmail.com>
2017-05-12 20:41:55 -0400

    remove console logging

--- google-analytics-mp.js
+++ google-analytics-mp.js
@@ -6,10 +6,10 @@ var lastPage;
 function calcURL(event) {
   console.log(event)
   var str= 'v=1' +
-//    '&ds=web' +
+    '&ds=web' +
     `&tid=${config.trackingId}` +
     `&cid=${config.clientId}` +
-//    `&ua=${encodeURI(config.userAgent)}` +
+    `&ua=${encodeURI(config.userAgent)}` +
     (config.userId ? `&uid=${config.userId}` : '') +
     `&t=${event.hitType}` +
     (event.page ? `&dp=${event.page}` : `&dp=${lastPage}`) + // Note use of lastPage
@@ -36,24 +36,21 @@ function setConfig(newConfig) {
   config = newConfig;
 }
 
-
 module.exports = { GoogleAnalyticsMeasurementProtocol, setConfig };
-
-
 
 function XhrPost(data) {
   var request = new XMLHttpRequest();
   request.onreadystatechange = function () {
-    console.log('onreadystatechange');
-    console.log(request.status);
-    console.log(request.responseText);
+//    console.log('onreadystatechange');
+//    console.log(request.status);
+//    console.log(request.responseText);
 //    console.log(request.responseType)
   };
   var url = 'http://www.google-analytics.com/collect?' + data.url;
   request.open("POST", url, true);
   request.send('');
-  console.log('post')
-  console.log(url)
+//  console.log('post')
+//  console.log(url)
 }
 
 module.exports = {
